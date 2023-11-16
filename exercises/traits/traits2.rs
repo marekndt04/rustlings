061b1b9891beef947a6e2067797349aafeaa1be7
Marek <m.arendt04@gmail.com>
2023-11-16 21:52:14 +0100

    Traits exercises and quiz3 done.

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,14 +11,18 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
-
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        let mut bar: Vec<String> = vec![String::from("Bar")];
+        self.append(&mut bar);
+        self
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
