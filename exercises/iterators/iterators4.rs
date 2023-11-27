0aac232dafd4a6e0bcc6195a3734a66367bba053
Marek <m.arendt04@gmail.com>
2023-11-27 18:15:30 +0100

    Iterators exercises done.

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,18 +1,18 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+// pub fn factorial(num: u64) -> u64 {
+//     if num == 0 {
+//         1
+//     } else {
+//         num * factorial(num - 1)
+//     }
+// }
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    let my_vec: Vec<u64> = (1..=num).collect();
+    my_vec.iter().fold(1, |acc, &x| acc * x)
 }
 
 #[cfg(test)]
